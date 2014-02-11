ddab986862a798af29b52bd9c5ee95dafd74e557
Valerie Montoya <valerie.montoya@cifacom.com>
2014-02-11 21:40:25 +0100

    fix page home

--- home.php
+++ home.php
@@ -11,25 +11,25 @@
 
 <body>
 	<div data-role="page" id="page1" data-theme="b">
-		<div data-role="header" id="header">
-			<input class="div_header" data-enhanced="true" value="Menu" id="menu" type="button"/>
-			<div class="div_header"><img src="/img/logo2012.png" alt="DailyMov" width="320" height="89" /></div>
-			<input class="div_header" data-enhanced="true" value="Tchat" type="button"/>
+		<div data-role="header" id="header" style="text-align:center;">
+			<div class="div_header" style="display:inline-block; vertical-align:top;"><input data-enhanced="true" value="Menu" id="menu" type="button"/></div>
+			<div class="div_header" style="display:inline-block; vertical-align:middle; margin: 0% 10% "><img src="/img/logo2012.png" alt="DailyMov" width="320" height="89" /></div>
+			<div class="div_header" style="display:inline-block; vertical-align:top;"><input data-enhanced="true" value="Tchat" type="button"/></div>
 		</div>
 		<div data-role="content" class="ui-content" data-theme="c">
 			<div id="droite" data-theme="a">
 				<ul data-role="listview" >
-					<li><a href="acura.html">Accueil</a></li>
-					<li><a href="audi.html">Profil</a></li>
-					<li><a href="bmw.html">Tchat</a></li>
-					<li><a href="audi.html">Réglages</a></li>
-					<li><a href="bmw.html">Partager sur FB</a></li>
+					<li><a href="">Accueil</a></li>
+					<li><a href="">Profil</a></li>
+					<li><a href="">Tchat</a></li>
+					<li><a href="">Réglages</a></li>
+					<li><a href="">Partager sur FB</a></li>
 				</ul>
 			</div>
 			<div id="gauche" style="text-align:center;">
 				<img class="photoProfil" src="Photo-profil.png" alt="image_logo" style="width:50%;"/>
-				<a href="index.html" data-role="button">Voir Film</a>
-				<a href="index.html" data-role="button">Proposition en attente</a>
+				<a href="" data-role="button">Voir Film</a>
+				<a href="" data-role="button">Proposition en attente</a>
 			</div>
 		</div>
 	</div>
