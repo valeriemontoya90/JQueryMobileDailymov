b6883f6c54b38037e2361d9478e840e881ec4542
Valerie Montoya <valerie.montoya@cifacom.com>
2014-02-11 23:08:02 +0100

    fix page proposer_profil_personne

---
+++ proposer_profil_personne.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<title>DailyMov</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
+</head>
+
+<body>
+	<div data-role="page" id="page1" data-theme="b">
+		<div data-role="header" id="header" style="text-align:center;">
+			<div class="div_header" style="display:inline-block; vertical-align:top;"><input data-enhanced="true" value="Menu" id="menu" type="button"/></div>
+			<div class="div_header" style="display:inline-block; vertical-align:middle; margin: 0% 10% "><img src="/img/logo2012.png" alt="DailyMov" width="320" height="89" /></div>
+			<div class="div_header" style="display:inline-block; vertical-align:top;"><input data-enhanced="true" value="Tchat" type="button"/></div>
+		</div>
+		<div data-role="content" class="ui-content" data-theme="c">
+			<div id="droite" data-theme="a">
+				<ul data-role="listview" >
+					<li><a href="">Accueil</a></li>
+					<li><a href="">Profil</a></li>
+					<li><a href="">Tchat</a></li>
+					<li><a href="">Réglages</a></li>
+					<li><a href="">Partager sur FB</a></li>
+				</ul>
+			</div>
+			<div id="gauche" style="text-align:center;">
+				<img class="photoProfil" src="Photo-profil.png" alt="image_logo" style="width:50%;"/>
+				<a href="" data-role="button">Proposer</a>
+			</div>
+		</div>
+	</div>
+	
+	<script type="text/javascript">
+		$(function(){
+			$("#page1 #droite").hide();
+			$("#page1 #menu").click(function(){
+				if ($("#page1 #droite").is(":hidden")) {
+					$("#page1 #droite").show();
+				} else {
+					$("#page1 #droite").hide();
+				}
+			});
+		});
+	</script>
+	
+	<!--<style type="text/css">
+
+#page1 .ui-content #droite{
+	visibility: visible;
+	z-index:10;
+	display:inline-block;
+}
+
+#page1 .ui-content #gauche{
+	display:inline-block;
+	z-index:-10;
+}
+	
+#page1 .ui-content #gauche .photoProfil{
+	width:50%;
+	height:50%;
+	left:20%;
+}
+	</style>-->
+</body>
+</html>
--- propositionsFilms.php
+++ propositionsFilms.php
@@ -2,45 +2,63 @@
 <html>
 <head>
 	<title>DailyMov</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="//code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css" />
-	<link rel="stylesheet" href="style.css" />
-	<script src="jquery-1.10.2.min"></script>
-	<script src="jquery.mobile-1.4.0"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
 </head>
 
 <body>
-	<div data-role="page" id="page1">
-	<div data-role="header" id="header">
-		<button><img alt="menu"/></button>
-		<div id="nomAppli"><h1>DailyMov</h1></div>
-		<button><img alt="tchat"/></button>
-	</div>
-	<div role="main" class="ui-content">
-		<ul data-role="listview" data-inset="true">
-			<li data-role="list-divider">Friday, October 8, 2010 <span class="ui-li-count">2</span></li>
-			<li><a href="index.html">
-			<h2>Stephen Weber</h2>
-			<p><strong>You've been invited to a meeting at Filament Group in Boston, MA</strong></p>
-			<p>Hey Stephen, if you're available at 10am tomorrow, we've got a meeting with the jQuery team.</p>
-				<p class="ui-li-aside"><strong>6:24</strong>PM</p>
-			</a></li>
-			<li><a href="index.html">
-			<h2>jQuery Team</h2>
-			<p><strong>Boston Conference Planning</strong></p>
-			<p>In preparation for the upcoming conference in Boston, we need to start gathering a list of sponsors and speakers.</p>
-				<p class="ui-li-aside"><strong>9:18</strong>AM</p>
-			</a></li>
-			<li data-role="list-divider">Thursday, October 7, 2010 <span class="ui-li-count">1</span></li>
-			<li><a href="index.html">
-			<h2>Avery Walker</h2>
-			<p><strong>Re: Dinner Tonight</strong></p>
-			<p>Sure, let's plan on meeting at Highland Kitchen at 8:00 tonight. Can't wait!</p>
-				<p class="ui-li-aside"><strong>4:48</strong>PM</p>
-			</a></li>
-		</ul>
-	</div>
-	<!--<div data-role="footer">...</div>-->
+	<div data-role="page" id="page1" data-theme="b">
+		<div data-role="header" id="header" style="text-align:center;">
+			<div class="div_header" style="display:inline-block; vertical-align:top;"><input data-enhanced="true" value="Menu" id="menu" type="button"/></div>
+			<div class="div_header" style="display:inline-block; vertical-align:middle; margin: 0% 10% "><img src="/img/logo2012.png" alt="DailyMov" width="320" height="89" /></div>
+			<div class="div_header" style="display:inline-block; vertical-align:top;"><input data-enhanced="true" value="Tchat" type="button"/></div>
+		</div>
+		<div role="content" class="ui-content" data-theme="c" >
+			<ul data-role="listview" data-inset="true">
+				<!--<li> data-role="list-divider"
+					<a href="#">
+						<img class="photoProfil" src="Photo-profil.png" alt="image_logo" style="width:50%;" class="ui-li-has-thumb ui-shadow"/>
+						<h3>Stephen Weber</h3>
+						<p><strong>Homme</strong> 25 ans</p>
+					</a>
+				</li>-->
+				<li data-role="list-divider">
+					<a href="#" style="text-decoration:none; color:black; vertical-align:top;">
+						<div style="display:inline-block; margin:0px; padding:0px">
+							<img class="photoProfil" src="Photo-profil.png" alt="image_logo" style="width:50%;"/>
+						</div>
+						<div style="display:inline-block; vertical-align:top;" >
+							<h3>Stephen Weber</h3>
+							<p><strong>Homme</strong> 25 ans</p>
+						</div>
+					</a>
+				</li>
+				<li data-role="list-divider">
+					<a href="#" style="text-decoration:none; color:black; vertical-align:top;">
+						<div style="display:inline-block; margin:0px; padding:0px">
+							<img class="photoProfil" src="Photo-profil.png" alt="image_logo" style="width:50%;"/>
+						</div>
+						<div style="display:inline-block; vertical-align:top;" >
+							<h3>Stephen Weber</h3>
+							<p><strong>Homme</strong> 25 ans</p>
+						</div>
+					</a>
+				</li>
+				<li data-role="list-divider">
+					<a href="#" style="text-decoration:none; color:black; vertical-align:top;">
+						<div style="display:inline-block; margin:0px; padding:0px">
+							<img class="photoProfil" src="Photo-profil.png" alt="image_logo" style="width:50%;"/>
+						</div>
+						<div style="display:inline-block; vertical-align:top;" >
+							<h3>Stephen Weber</h3>
+							<p><strong>Homme</strong> 25 ans</p>
+						</div>
+					</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </body>
 </html>
